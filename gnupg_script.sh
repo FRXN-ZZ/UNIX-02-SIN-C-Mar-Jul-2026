477867b3f3e381569b25fff1141b572293a85604
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-03-23 15:45:19 +0000

    documentos cifrados

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -3,3 +3,6 @@
 # gpg --armor --export-secret-keys FE4931AAC42C6434 exporta la llave secreta
 # gpg --import llave_publica_de_Cris.asc para compartir mi llave publica
 # gpg --list-keys lista de llaves incluida de mi amigo
+# echo "Hola hehehe" > doc_no_cifrado.exe mensaje para nuestro compa
+# cat doc_no_cifrado.txt #verificar el comentario enviado a nuestro compa
+# gpg --output doc_no_cifrado.txt --encrypt --recipient XXXXXXXXXXXXXXXXXXXXXXXXXX doc_no_cifrado.txt CIFRA EL DOCUMENTO

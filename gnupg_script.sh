e7f89fb8b03060d130732134ffc66332f02e69b9
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-03-23 15:11:32 +0000

    llave importada de mi compa

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,5 @@
 # FE4931AAC42C6434 llave
 # gpg --list-secret-keys --keyid-format=long comando para lista privada 
 # gpg --armor --export-secret-keys FE4931AAC42C6434 exporta la llave secreta
+# gpg --import llave_publica_de_Cris.asc para compartir mi llave publica
+# gpg --list-keys lista de llaves incluida de mi amigo

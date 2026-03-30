419c2066d7b64383db265816faa74d86ca36cca7
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-03-30 15:06:28 +0000

    install parted

---
+++ Exploration_script.sh
@@ -0,0 +1,3 @@
+# sudo apt update Use the URLs where I'm going to download the libraries
+# sudo apt upgrade This command updates all the software already installed on your system to newer versions
+# sudo apt install parted This command installs GNU Parted, which is used to create, delete, and manage disk partitions.

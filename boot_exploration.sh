4ee45a9dceb15efb421e3456f1fd89626ad62743
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-04-17 15:03:28 +0000

    chmod

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -1,10 +1,9 @@
 #List all the directories
 ls -a #Short option
 ls --all #Long option
-#.          Darwin_llave.asc                calculadora                     doc_cifrado_darwinAl.txt        doc_no_cifradoDarwinAl.txt      final_cifrado_firmadoDarwinAl.txt.gpg
-#..         LICENSE                         cifrado_firmado_final.txt.gpg   doc_no_cifrado.txt              doc_no_cifradoDarwinAl.txt.gpg  gnupg_script.sh
-#.git       README.md                       da_llave_publica.asc            doc_no_cifrado.txt.gpg          doc_no_cifradoDarwinAl.txt.sig  mi_clave_publica.asc
-#.gitignore boot_exploration.sh             doc_cifrado.txt                 doc_no_cifrado.txt.sig          doc_no_cifrado_firmado.txt
+.     .gitignore  boot_exploration.sh  doc_cris_.txt         doc_cris_firmado.gpg   doc_no_cifrado.txt      doc_no_cifrado.txt.sig  llave_publica_de_Cris.asc  mensaje_recibido.txt
+..    LICENSE     calculadora          doc_cris_.txt.gpg     doc_cris_firmado.sig   doc_no_cifrado.txt.asc  filesystem_script.sh    mensaje_original.txt       mi_llave_publica-asc
+.git  README.md   doc_cifrado.txt      doc_cris_firmado.asc  doc_cris_original.txt  doc_no_cifrado.txt.gpg  gnupg_script.sh         mensaje_original.txt.gpg   prueba_final.txt
 
 ls -l -a -h #The command ls -lah is used to list every file in a folder with full details
 ls -l -ah #Short option
@@ -27,3 +26,25 @@ rmdir -- -rf #Deletes the directory named -rf
 ls --help #displays a comprehensive list of all the options you can use with the ls.
 ls --help #displays a comprehensive list of all the options you can use with the ls.
 man ls #Opens a full, paginated documentation file that explains everything about the ls command.
+
+ --depth <depth>
+           Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If
+           you want to clone submodules shallowly, also pass --shallow-submodules.
+
+ls -l
+-rw-rw-rw-  1 codespace root      34523 Apr 17 14:30 LICENSE
+
+-rw-rw-rw-  1 codespace root  34523 Apr 17 14:30 LICENSE
+|          | |         |     |     |            |
+|          | |         |     |     |         File Name
+|          | |         |     |    Modification Date
+|          | |         |  File Size (Bytes)
+|          | |       Group
+|          | Owner
+|     Number of Links
+File Permissions
+
+chmod +x script.sh          # everyone can execute it (equivalent to a+x)
+chmod u+x script.sh         # only the owner can execute it
+chmod o-r secret.txt        # remove read permission from "others"
+chmod u+rw,go-rwx private   # owner can read/write; no one else has any permissions

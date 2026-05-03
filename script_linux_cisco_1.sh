a0e7387ee4575b0c23d0f12b3100849d24b18464
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-05-03 22:28:48 +0000

    COMMANDS WITH OPTIONS

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -7,3 +7,9 @@ Documents
 # Description: Lists the contents of the Documents directory.
 aptitude moo
 # Description: A package management command with a funny easter egg argument.
+
+# COMMANDS WITH OPTIONS 
+ls -l
+# Description: Lists contents in long format
+ls -r
+# Description: Lists contents in reverse alphabetical order.

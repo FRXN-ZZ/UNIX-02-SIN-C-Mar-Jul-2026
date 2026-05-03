b53ea1d6e5068a96a8b71f93430756324e127377
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-05-03 22:26:24 +0000

    COMMANDS WITH ARGUMENTS

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,9 @@
 # BASIC COMMAND SYNTAX
 ls
 # Description: Lists directory contents.
+
+# COMMANDS WITH ARGUMENTS
+Documents
+# Description: Lists the contents of the Documents directory.
+aptitude moo
+# Description: A package management command with a funny easter egg argument.

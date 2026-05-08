1b25c2c328a783a1d6146d8e527b6cb45ba42892
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-05-08 15:03:35 +0000

    touch

--- script.zsh
+++ script.zsh
@@ -23,3 +23,19 @@ head
 touch test.txt 
 ls- la
 # My files and directoris inherit the gid and vid
+cat /etc/group | head -10
+cat 
+# Displays the contents of a file.
+/etc/group
+#Linux system file that stores group information.
+| 
+# Sends the output of one command to another command.
+head -10 
+# Displays the first 10 lines of the file.
+groups
+# Displays all groups the current user belongs to.
+groups USER
+groups 
+# Displays the groups of a specific user.
+USER 
+# Replace with an actual username

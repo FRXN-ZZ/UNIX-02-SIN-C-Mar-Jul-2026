818e091442611377ccdbac7d1c31419e95db2db4
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-05-08 14:47:27 +0000

    touch

---
+++ script.zsh
@@ -0,0 +1,22 @@
+ls	
+# Shows files and folders (Lists).
+clear	
+# Clears the visual history of the terminal.
+id	
+#Shows your User and Group IDs (UID/GID).
+groups	
+#Lists the names of the groups you are in.
+cat /etc/passwd
+cat
+# Displays the contents of a file.
+/etc/passwd 
+# Linux system file that stores user account information.
+cat /etc/passwd | head
+cat 
+# Displays the contents of a file.
+/etc/passwd 
+# Linux system file containing user information.
+| 
+# Sends the output of one command to the next command.
+head 
+# Shows only the first lines (10 by default).

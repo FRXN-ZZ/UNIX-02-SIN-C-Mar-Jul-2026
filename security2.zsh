f99e880bb1099dbaab9995c18c09f96f238d2b59
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-05-29 15:09:23 +0000

    commic2

--- security2.zsh
+++ security2.zsh
@@ -1,13 +1,14 @@
 # View the current primary group
-
 id
-
 id -gn    # only the name of the primary group
-
 # Create a file and see which group it inherits
-
 touch ~/test_inherited_group.txt
-
 ls -la ~/test_inherited_group.txt
-
 # The group is the user's primary group
+
+# Show the current group
+id -gn
+
+# Create a file before using newgrp
+touch ~/before_newgrp.txt
+ls -la ~/before_newgrp.txt

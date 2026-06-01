4d74569da4fd11797ba02502aaf7831d9ae5a7d0
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-06-01 15:20:54 +0000

    Debugging

--- .devcontainer/blackhatbash1.sh
+++ .devcontainer/blackhatbash1.sh
@@ -10,4 +10,10 @@ df --human-readable
 bash -x
 # When we execute these commands, we enter a loop
 bash -r
-# bash script.sh
+bash script.sh
+# Runs the script in your current folder
+./blackhatbash1.sh
+# Gives you permission to execute it.
+chmod u+x blackhatbash1.sh
+set-x
+set+x

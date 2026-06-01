18c7c98d37bea8fbb4a08f7287877501303b2108
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-06-01 15:12:50 +0000

    The Shebang Line

--- .devcontainer/blackhatbash1.sh
+++ .devcontainer/blackhatbash1.sh
@@ -4,3 +4,10 @@ echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -e -f 
+ps -ef
+df --human-readable
+bash -x
+# When we execute these commands, we enter a loop
+bash -r
+# bash script.sh

37d44f89ad284725cb52b67687ad2b492870bd0f
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-06-05 15:45:31 +0000

    WIP

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -12,3 +12,12 @@ echo "Goodbye!" >> output.txt
 cat output.txt
 #Hello World! 
 #Goodbye!
+lzl 2> error.txt
+error de gato.txt
+cat < output.txt
+cat << EOF 
+Black Hat Bash 
+by No Starch Press 
+EOF 
+#Black Hat Bash 
+#by No Starch Press

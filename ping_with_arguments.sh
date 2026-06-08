fe254de4351f0bcf7f1bd343f56e1e14759ab5af
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-06-08 15:26:45 +0000

    Positional Arguments 1.1

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-# This script will ping any address provided as an argument. 
 
+# This script will ping any address provided as an argument.
 SCRIPT_NAME="${0}"
-TARGET="${127.0.0.1}" 
+TARGET="${1:-127.0.0.1}" 
 
 echo "Running the script ${SCRIPT_NAME}..." 
 echo "Pinging the target: ${TARGET}..." 
 
-ping "${TARGET}"
+ping -c 4 "${TARGET}"
+chmod u+x ping_with_arguments.sh
+chmod u+x ping_with_arguments.sh
+
+echo "The arguments are: $ @" 
+echo "The total number of arguments is: $#"
+
+chmod u+x show_args.sh 
+./show_args.sh "hello" "world"
+# Change "$@" to "$*" to observe behavior. 
+for args in "$@"; do 
+    echo "${args}" 
+done

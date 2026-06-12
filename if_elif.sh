501b3c77bbe10237921b54bfe9be995a1ca5ae98
FRXN-ZZ <edmuelagu@uide.edu.ec>
2026-06-12 14:39:42 +0000

    Checking Subsequent Conditions: COMENTARY

--- if_elif.sh
+++ if_elif.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 USER_INPUT="${1}"
 
 if [[ -z "${USER_INPUT}" ]]; then
@@ -14,3 +13,8 @@ elif [[ -d "${USER_INPUT}" ]]; then
 else
     echo "${HOLA} is not a file or a directory."
 fi
+
+#COMENTARY
+#This script validates the argument you pass when running it to determine its nature: 
+#first, it checks if you forgot to input any data, in which case it stops the program with an error message; 
+#if the argument exists, it checks in an orderly fashion whether it corresponds to the path of an existing file or a directory (folder), printing the confirmation on the screen, and finally, if it matches neither option or the path does not exist, it displays a message indicating that the item could not be classified.
